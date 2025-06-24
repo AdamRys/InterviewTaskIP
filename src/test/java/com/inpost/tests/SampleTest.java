c240cf01c9e494b360e56c621921f94d57307456
AdamRyś <biggest.lynx@gmail.com>
2025-06-24 15:58:05 +0200

    Initial project setup

---
+++ src/test/java/com/inpost/tests/SampleTest.java
@@ -0,0 +1,11 @@
+package com.inpost.tests;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void sample() {
+        System.out.println("Environment works");
+    }
+}
